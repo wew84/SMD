9a1f7d9e36d63e45b452dc75492602c8f29487d9
unknown <chais@WS-PM6.IASA.ORG>
2015-05-28 21:25:03 +0300

    Fleshed out courses listing

---
+++ client/courses/courseEdit.js
@@ -0,0 +1,33 @@
+Template.courseEdit.helpers = {
+  isNewCourse: function() {
+    return this._id == null || this._id == "null";
+  }
+}
+Template.courseEdit.events = {
+  "submit form": function(e) {
+    e.preventDefault();
+    var courseUpdateProperties = {
+      name: $(e.target).find("[name=nameinput]").val(),
+      grade: $(e.target).find("[name=gradeselect]:checked").val() == "true"
+    };
+    if(this._id == null || typeof this._id == "undefined"){
+      Meteor.call('insertCourse', courseUpdateProperties, log_error);
+    } else {
+      Meteor.call('updateCourse', this._id, courseUpdateProperties, log_error);
+    }
+   
+  },
+
+  "click #delete-button": function(e) {
+    e.preventDefault();
+    Meteor.call('deleteCourse', this._id, log_error);
+  }
+}
+
+log_error = function(error, id) {
+    if(error) {
+    console.log(error);
+  } else {
+    Router.go('courseList');
+  }
+}
