86a8562e6115d42a9e1e3a1cb56a6234a8a7f923
Noam Golombek <ngolombek@gmail.com>
2015-05-29 02:37:10 +0300

    Settings added

--- client/courses/courseList.js
+++ client/courses/courseList.js
@@ -10,5 +10,17 @@ Template.courseList.events = {
   "click #create-button": function(e) {
     e.preventDefault();
     Router.go("courseEdit", {_id: null});
+  },
+  "click #courseChange": function(e) {
+  	e.preventDefault();
+
+  	var courses = _.pluck($.find(".utility-checkbox:checked"), "data-id");
+  	Meteor.call("editUser", courses, function(error) {
+      if(error){
+        toastr.error(error.reason);
+      } else {
+      	
+      }
+    });
   }
 };
